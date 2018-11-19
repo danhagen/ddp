d1a7317e0ec52d641b192e0e67866656d67a954f
dhagen.work <dhagen@usc.edu>
2018-11-19 13:51:44 -0800

    found error in EOM, fixed forward_intergration code

--- ddp_pendulum_cart_example.py
+++ ddp_pendulum_cart_example.py
@@ -14,6 +14,8 @@ X2: position of the cart
 X3: angular velocity of pendulum
 X4: velocity of the cart
 
+dt should be set smaller for this problem as integration error can effect the behavior. See what happens for forward_integrate_dynamics([170,0,0,0],dt=0.01). Try dt = 0.0001
+
 """
 def dx1_dt(X,U):
     return(X[2])
@@ -24,7 +26,7 @@ def dx3_dt(X,U):
         (
             m2*L*np.cos(X[0])
                 * (
-                    b1*X[3] - m2*L*np.cos(X[0])*X[2]**3 - U
+                    b1*X[3] - m2*L*np.sin(X[0])*X[2]**3 - U
                 )
             + (m1+m2)*(m2*g*L*np.sin(X[0]) - b2*X[2])
         )
@@ -38,7 +40,7 @@ def dx4_dt(X,U):
         (
             m2*L**2
                 * (
-                    m2*L*np.cos(X[0])*X[2]**3 - b1*X[3] + U
+                    m2*L*np.sin(X[0])*X[2]**3 - b1*X[3] + U
                 )
             + m2*L*np.cos(X[0])
                 * (b2*X[2] - m2*g*L*np.sin(X[0]))
@@ -130,35 +132,310 @@ def forward_integrate_dynamics(
         if Animate:
             animate_trajectory(Time,X,U)
         else:
-            plt.figure(figsize=(15,4))
+            plt.figure(figsize=(15,10))
 
-            ax1 = plt.subplot(221)
+            ax1 = plt.subplot(323)
             ax1.plot(Time,180*X[0,:]/np.pi,'g')
             ax1.set_xlabel('Time (s)')
             ax1.set_ylabel('Pendulum Angle (deg)')
             if max(abs(180*X[0,:]/np.pi - 180*X[0,0]/np.pi))<1e-7:
                 ax1.set_ylim([180*X[0,0]/np.pi - 5,180*X[0,0]/np.pi + 5])
 
-            ax2 = plt.subplot(222)
+            ax2 = plt.subplot(324)
             ax2.plot(Time,X[1,:],'r')
             ax2.set_xlabel('Time (s)')
             ax2.set_ylabel('Cart Position (m)')
             if max(abs(X[1,:] - X[1,0]))<1e-7:
                 ax2.set_ylim([X[1,0] - 5,X[1,0] + 5])
 
-            ax3 = plt.subplot(223)
+            ax3 = plt.subplot(325)
             ax3.plot(Time,180*X[2,:]/np.pi,'g--')
             ax3.set_xlabel('Time (s)')
             ax3.set_ylabel('Pendulum Angular \n Velocity (deg/s)')
             if max(abs(180*X[2,:]/np.pi-180*X[2,0]/np.pi))<1e-7:
                 ax3.set_ylim([180*X[2,0]/np.pi-1,180*X[2,0]/np.pi+1])
 
-            ax4 = plt.subplot(224)
+            ax4 = plt.subplot(326)
             ax4.plot(Time,X[3,:],'r--')
             ax4.set_xlabel('Time (s)')
             ax4.set_ylabel('Cart Velocity (m/s)')
             if max(abs(X[3,:] - X[3,0]))<1e-7:
                 ax4.set_ylim([X[3,0] - 5,X[3,0] + 5])
+
+            # ax5 = plt.subplot2grid((3,2),(0,0),colspan=2)
+            ax5 = plt.subplot(322)
+            ax5.plot(Time[:-1],U[:],'b')
+            ax5.set_xlabel('Time (s)')
+            ax5.set_ylabel('Input Force (N)')
+            if max(abs(U[:] - U[0]))<1e-7:
+                ax5.set_ylim([U[0] - 5,U[0] + 5])
+
+            # ax0 = plt.subplot2grid((3,4),(0,0),colspan=2) # animation
+            ax0 = plt.subplot(321)
+
+            Pendulum_Width = 0.01*L
+            Pendulum_Length = 2*L
+            Cart_Width = 4*L
+            Cart_Height = 2*L
+            Wheel_Radius = 0.125*Cart_Width
+
+            Cart = plt.Rectangle(
+                        (-Cart_Width/2,-Cart_Height/2),
+                        Cart_Width,
+                        Cart_Height,
+                        Color='#4682b4')
+            ax0.add_patch(Cart)
+
+            FrontWheel = plt.Circle(
+                        (Cart_Width/4,-(Cart_Height/2 + Wheel_Radius)),
+                        Wheel_Radius,
+                        Color='k')
+            ax0.add_patch(FrontWheel)
+            FrontWheel_Rivet = plt.Circle(
+                        (Cart_Width/4,-(Cart_Height/2 + Wheel_Radius)),
+                        0.2*Wheel_Radius,
+                        Color='0.70')
+            ax0.add_patch(FrontWheel_Rivet)
+
+            BackWheel = plt.Circle(
+                        (-Cart_Width/4,-(Cart_Height/2 + Wheel_Radius)),
+                        Wheel_Radius,
+                        Color='k')
+            ax0.add_patch(BackWheel)
+            BackWheel_Rivet = plt.Circle(
+                        (-Cart_Width/4,-(Cart_Height/2 + Wheel_Radius)),
+                        0.2*Wheel_Radius,
+                        Color='0.70')
+            ax0.add_patch(BackWheel_Rivet)
+
+            Pendulum, = ax0.plot(
+                            [
+                                0,
+                                Pendulum_Length*np.sin((30*np.pi/180))
+                            ],
+                            [
+                                Cart_Height/2 + Pendulum_Width/2,
+                                Cart_Height/2 + Pendulum_Width/2 + Pendulum_Length*np.cos((30*np.pi/180))
+                            ],
+                            Color='0.50',
+                            lw = 10,
+                            solid_capstyle='round'
+                            )
+
+            Pendulum_neutral, = ax0.plot(
+                            [
+                                0,
+                                0
+                            ],
+                            [
+                                Cart_Height/2 + Pendulum_Width/2,
+                                Cart_Height/2 + Pendulum_Width/2 + Pendulum_Length
+                            ],
+                            Color='k',
+                            lw = 1,
+                            linestyle='--'
+                            )
+
+            Angle_indicator, = ax0.plot(
+                            Pendulum_Length*np.sin(
+                                np.linspace(0.05*(30*np.pi/180),0.95*(30*np.pi/180),20)
+                                ),
+                            Cart_Height/2
+                            + Pendulum_Width/2
+                            + Pendulum_Length*np.cos(
+                                np.linspace(0.05*(30*np.pi/180),0.95*(30*np.pi/180),20)
+                                ),
+                            Color='g',
+                            lw = 2,
+                            solid_capstyle = 'round'
+                            )
+            k = 0.15*Pendulum_Length
+            Angle_indicator_arrow, = ax0.plot(
+                            Pendulum_Length*np.sin(0.95*(30*np.pi/180))
+                            + [
+                                -k*np.cos(0.95*(30*np.pi/180) + (30*np.pi/180)),
+                                0,
+                                -k*np.cos(0.95*(30*np.pi/180) - (30*np.pi/180))
+                            ],
+                            Cart_Height/2
+                            + Pendulum_Width/2
+                            + Pendulum_Length*np.cos(0.95*(30*np.pi/180))
+                            + [
+                                k*np.sin(0.95*(30*np.pi/180) + (30*np.pi/180)),
+                                0,
+                                k*np.sin(0.95*(30*np.pi/180) - (30*np.pi/180))
+                            ],
+                            Color='g',
+                            lw = 2,
+                            solid_capstyle='round'
+                            )
+            Angle_damping_indicator, = ax0.plot(
+                            0.5*Pendulum_Length*np.sin(
+                                    np.linspace(
+                                        1.05*(30*np.pi/180),
+                                        1.05*(30*np.pi/180)+(45*np.pi/180),
+                                        20
+                                    )
+                                ),
+                            Cart_Height/2
+                            + Pendulum_Width/2
+                            + 0.5*Pendulum_Length*np.cos(
+                                    np.linspace(
+                                        1.05*(30*np.pi/180),
+                                        1.05*(30*np.pi/180)+(45*np.pi/180),
+                                        20
+                                    )
+                                ),
+                            Color='#ffa500',
+                            lw = 2,
+                            solid_capstyle = 'round'
+                            )
+            Angle_damping_indicator_arrow, = ax0.plot(
+                            0.5*Pendulum_Length*np.sin(1.05*(30*np.pi/180))
+                            + [
+                                -k*np.cos(1.05*(30*np.pi/180) + (30*np.pi/180) + np.pi),
+                                0,
+                                -k*np.cos(1.05*(30*np.pi/180) - (30*np.pi/180) + np.pi)
+                            ],
+                            Cart_Height/2
+                            + Pendulum_Width/2
+                            + 0.5*Pendulum_Length*np.cos(1.05*(30*np.pi/180))
+                            + [
+                                k*np.sin(1.05*(30*np.pi/180) + (30*np.pi/180) + np.pi),
+                                0,
+                                k*np.sin(1.05*(30*np.pi/180) - (30*np.pi/180) + np.pi)
+                            ],
+                            Color='#ffa500',
+                            lw = 2,
+                            solid_capstyle='round'
+                            )
+
+            Position_damping_indicator, = ax0.plot(
+                            [0.55*Cart_Width,0.90*Cart_Width],
+                            [0]*2,
+                            Color='#800080',
+                            lw = 2,
+                            solid_capstyle = 'round'
+                            )
+            Position_damping_indicator_arrow, = ax0.plot(
+                            0.55*Cart_Width
+                            - np.array([
+                                -k*np.cos((30*np.pi/180)),
+                                0,
+                                -k*np.cos(-(30*np.pi/180))
+                            ]),
+                            - np.array([
+                                k*np.sin((30*np.pi/180)),
+                                0,
+                                k*np.sin(-(30*np.pi/180))
+                            ]),
+                            Color='#800080',
+                            lw = 2,
+                            solid_capstyle='round'
+                            )
+
+
+            Pendulum_Attachment = plt.Circle((0,Cart_Height/2),100*Pendulum_Width/2,Color='#4682b4')
+            ax0.add_patch(Pendulum_Attachment)
+
+            Pendulum_Rivet, = ax0.plot(
+                [0],
+                [Cart_Height/2 + Pendulum_Width/2],
+                c='k',
+                marker='o',
+                lw=2
+                )
+
+            Ground = plt.Rectangle(
+                        (-10,-1.50*(Cart_Height/2 + Wheel_Radius*2)),
+                        20,
+                        0.50*(Cart_Height/2 + Wheel_Radius*2),
+                        Color='0.70')
+            ax0.add_patch(Ground)
+
+            Position_indicator, = ax0.plot(
+                            [-k,5*k],
+                            [-1.25*(Cart_Height/2 + Wheel_Radius*2)]*2,
+                            Color='r',
+                            lw = 2,
+                            solid_capstyle = 'round'
+                            )
+            Position_indicator_cross, = ax0.plot(
+                            [0,0],
+                            [
+                                -1.25*(Cart_Height/2 + Wheel_Radius*2)+k,
+                                -1.25*(Cart_Height/2 + Wheel_Radius*2)-k
+                            ],
+                            Color='r',
+                            lw = 2,
+                            solid_capstyle = 'round'
+                            )
+
+            Position_indicator_arrow, = ax0.plot(
+                            5*k
+                            + np.array([
+                                -k*np.cos((30*np.pi/180)),
+                                0,
+                                -k*np.cos(-(30*np.pi/180))
+                            ]),
+                            -1.25*(Cart_Height/2 + Wheel_Radius*2)
+                            + np.array([
+                                k*np.sin((30*np.pi/180)),
+                                0,
+                                k*np.sin(-(30*np.pi/180))
+                            ]),
+                            Color='r',
+                            lw = 2,
+                            solid_capstyle='round'
+                            )
+
+            Force_indicator, = ax0.plot(
+                            [-0.90*Cart_Width,-0.55*Cart_Width],
+                            [0]*2,
+                            Color='b',
+                            lw = 2,
+                            solid_capstyle = 'round'
+                            )
+            Force_indicator_arrow, = ax0.plot(
+                            -0.55*Cart_Width
+                            + np.array([
+                                -k*np.cos((30*np.pi/180)),
+                                0,
+                                -k*np.cos(-(30*np.pi/180))
+                            ]),
+                            np.array([
+                                k*np.sin((30*np.pi/180)),
+                                0,
+                                k*np.sin(-(30*np.pi/180))
+                            ]),
+                            Color='b',
+                            lw = 2,
+                            solid_capstyle='round'
+                            )
+
+            ax0.get_xaxis().set_ticks([])
+            ax0.get_yaxis().set_ticks([])
+            ax0.set_frame_on(True)
+            ax0.set_xlim([-1*Cart_Width,1*Cart_Width])
+            ax0.set_ylim(
+                [
+                    -1.50*(Cart_Height/2 + Wheel_Radius*2),
+                    1.25*(Cart_Height/2+Pendulum_Width/2+Pendulum_Length+Pendulum_Width/2)
+                ]
+            )
+
+            props = dict(boxstyle='round', facecolor='wheat', alpha=0.5)
+            # ax0.text(0.05, 0.95, r"$b_1$ = " + str(b1) + "\n" + r"$b_2$ = " + str(b2), transform=ax0.transAxes, fontsize=14,
+            # verticalalignment='top', bbox=props)
+            ax0.legend(
+                (Position_damping_indicator,Angle_damping_indicator),
+                (r"$b_1$ = " + str(b1), r"$b_2$ = " + str(b2)),
+                loc='upper left',
+                facecolor='wheat',
+                framealpha=0.5,
+                title="Damping")
+            # ax0.set_ylabel(r"$b_1$ = " + str(b1) + r"\n $b_2$ = " + str(b2))
+            ax0.set_aspect('equal')
 
             plt.show()
 
@@ -233,7 +510,7 @@ def animate_trajectory(Time,X,U,**kwargs):
                     )
 
 
-    Pendulum_Attachment = plt.Circle((X[1,0],Cart_Height/2),2*Pendulum_Width/2,Color='#4682b4')
+    Pendulum_Attachment = plt.Circle((X[1,0],Cart_Height/2),100*Pendulum_Width/2,Color='#4682b4')
     ax0.add_patch(Pendulum_Attachment)
 
     Pendulum_Rivet, = ax0.plot(
@@ -430,7 +707,7 @@ def animate_trajectory(Time,X,U,**kwargs):
                         solid_capstyle='round'
                         )
 
-        Pendulum_Attachment = plt.Circle((X[0,0],Cart_Height/2),2*Pendulum_Width/2,Color='#4682b4')
+        Pendulum_Attachment = plt.Circle((X[0,0],Cart_Height/2),100*Pendulum_Width/2,Color='#4682b4')
         ax0.add_patch(Pendulum_Attachment)
 
         Pendulum_Rivet, = ax0.plot(
@@ -486,9 +763,9 @@ def animate_trajectory(Time,X,U,**kwargs):
         return Cart,FrontWheel,FrontWheel_Rivet,BackWheel,BackWheel_Rivet,Pendulum,Pendulum_Attachment,Pendulum_Rivet,Ground,Input,Position,Angle,Velocity,
 
     dt = Time[1]-Time[0]
-    ani = animation.FuncAnimation(fig, animate, frames=np.arange(0,len(Time)-1,10), init_func=init, blit=False)
+    ani = animation.FuncAnimation(fig, animate, frames=np.arange(0,len(Time)-1,2000),init_func=init, blit=False)
     if SaveAsGif==True:
-        ani.save('simple_pendulum_ddp.gif', writer='imagemagick', fps=10)
+        ani.save('simple_pendulum_ddp_01.gif', writer='imagemagick', fps=10)
     plt.show()
 
 def return_f31(X,U):
@@ -1441,15 +1718,15 @@ m1 = 10 # kg
 m2 = 1 # kg
 L = 0.5 # m
 g = 9.8 # m/s²
-b1 = 1 # Nms
-b2 = 0 # kg/s
+b1 = 10 # kg/s - Damping coefficient for cart position
+b2 = 10 # Nms - Damping coefficient for pendulum angle
 
-k1 = 1000
-k2 = 1000
-k3 = 1
-k4 = 1
-k5 = 1
-k6 = 1
+k1 = 1000 # weight of "Minimize time away from target angle" in RunningCost
+k2 = 1000 # weight of "Minimize time away from target angular velocity" in RunningCost
+k3 = 1 # weight of "Minimize Input Energy" in RunningCost
+k4 = 1 # weight of 'Minimize final angle from target angle' in TerminalCost
+k5 = 1 # weight of  'Minimize final angular velocity from target angular velocity' in TerminalCost
+k6 = 1 # weight of "Minimize time away from initial position" in RunningCost
 
 TargetAngle = 0 #in radians
 TargetAngularVelocity = 0 #in radians
